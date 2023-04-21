cc16725da28d34b9d2896c2040a8a12f77aebaf6
CarlWollebekk <jlz172@econ.ku.dk>
2023-04-21 15:15:56 +0200

    done

--- inauguralproject/inauguralproject.py
+++ inauguralproject/inauguralproject.py
@@ -6,6 +6,7 @@ from scipy import optimize
 
 import pandas as pd
 import matplotlib.pyplot as plt
+import ipywidgets
 
 import math
 
@@ -55,7 +56,17 @@ class HouseholdSpecializationModelClass:
         # a. consumption of market goods
         C = par.wM*LM + par.wF*LF
 
-      # b. home production
+         # b. home production, first defining the power
+        if par.sigma == 0:
+            s_power = (par.sigma-1)/(par.sigma+1e-8)
+
+        elif par.sigma == 1:
+            s_power = 0
+
+        else: 
+            s_power = (par.sigma-1)/(par.sigma)
+
+        # Then defining the home production
         if par.sigma == 0:
             H = pd.min(HM,HF)
 
@@ -63,17 +74,29 @@ class HouseholdSpecializationModelClass:
             H = HM**(1-par.alpha)*HF**par.alpha
             
         else:
-            H = ((1-par.alpha)*HM**((par.sigma-1)/par.sigma+1e-8)+par.alpha*HF**((par.sigma-1)/par.sigma+1e-8))**(par.sigma/(par.sigma+1e-8-1))
+            H = ((1-par.alpha)*HM**(s_power)+par.alpha*HF**(s_power))**(1/s_power)
 
-        # c. total consumption utility
+        # c. total consumption utility, starting with defining the power
+        if par.rho == 1:
+            r_power = (1-par.rho+1e-8)
+
+        else:
+            r_power = (1-par.rho)
+
+        # Then doing the total consumption
         Q = C**par.omega*H**(1-par.omega)
-        utility = np.fmax(Q,1e-8)**(1-par.rho)/(1-par.rho)
+        utility = np.fmax(Q,1e-8)**(r_power)/(r_power)
 
-        # d. disutlity of work
-        epsilon_ = 1+1/par.epsilon
+        # d. disutlity of work, starting with defining the power
+        if par.epsilon == 0:
+            e_power = 1+1/(par.epsilon+1e-8)
+        else: 
+            e_power = 1+1/par.epsilon
+
+        # Then doing the disutility of work
         TM = LM+HM
         TF = LF+HF
-        disutility = par.nu*(TM**epsilon_/epsilon_+TF**epsilon_/epsilon_)
+        disutility = par.nu*(TM**e_power/e_power+TF**e_power/e_power)
         
         return utility - disutility
 
@@ -115,49 +138,61 @@ class HouseholdSpecializationModelClass:
 
         return opt
 
-    def solve(self,do_print=False):
+    def solve(self, do_print=False):
         """ solve model continously """
 
         par = self.par
         sol = self.sol
         opt = SimpleNamespace()
         
-        # a. all possible choices
-        x = np.linspace(0,24, 97)
-        LM,HM,LF,HF = np.meshgrid(x,x,x,x) # all combinations
-    
-        LM = LM.ravel() # vector
-        HM = HM.ravel()
-        LF = LF.ravel()
-        HF = HF.ravel()
+        # We provide an initial guess
+        LM_guess, LF_guess, HM_guess, HF_guess = 6,6,6,6
+        x_guess = np.array([LM_guess, LF_guess, HM_guess, HF_guess])
 
-        # b. calculate utility
-        u = self.calc_utility(LM,HM,LF,HF)
-    
-        # c. set to minus infinity if constraint is broken
-        I = (LM+HM > 24) | (LF+HF > 24) # | is "or"
-        u[I] = -np.inf
-    
-        # d. find maximizing argument
-        j = np.argmax(u)
-        
-        opt.LM = LM[j]
-        opt.HM = HM[j]
-        opt.LF = LF[j]
-        opt.HF = HF[j]
+        # We define the objective function
+        obj = lambda x: -self.calc_utility(x[0], x[1], x[2], x[3])
 
-        # e. print
+        # We set the bounds
+        bounds = ((1e-8, 24-1e-8),(1e-8, 24-1e-8),(1e-8, 24-1e-8),(1e-8, 24-1e-8))
+
+        # We optimize using the Nelder-Mead method
+        result = optimize.minimize(obj, x_guess, method="Nelder-Mead", bounds=bounds, tol=1e-8)
+
+        opt.LM = result.x[0]
+        opt.HM = result.x[1]
+        opt.LF = result.x[2]
+        opt.HF = result.x[3]
+
+         # e. print
         if do_print:
             for k,v in opt.__dict__.items():
                 print(f'{k} = {v:6.4f}')
 
         return opt  
 
-    def solve_wF_vec(self,discrete=False):
+    def solve_wF_vec(self,discrete=False, do_plot=False, do_print=False):
         """ solve model for vector of female wages """
 
-        pass
+        par = self.par 
+        sol = self.sol
 
+        for i, w in enumerate(self.par.wF_vec):
+            par.wF = w
+
+            if discrete:
+                opt = self.solve_discrete()
+
+            else:
+                opt = self.solve()
+
+            sol.LF_vec[i] = opt.LF
+            sol.HF_vec[i] = opt.HF
+            sol.LM_vec[i] = opt.LM
+            sol.HM_vec[i] = opt.HM
+
+        par.wF = 1.0
+
+    
     def run_regression(self):
         """ run regression """
 
@@ -168,10 +203,38 @@ class HouseholdSpecializationModelClass:
         y = np.log(sol.HF_vec/sol.HM_vec)
         A = np.vstack([np.ones(x.size),x]).T
         sol.beta0,sol.beta1 = np.linalg.lstsq(A,y,rcond=None)[0]
-        return(sol.beta0, sol.beta1)
         
-    
-    def estimate(self,alpha=None,sigma=None):
+
+    def estimate(self, do_print=False):
         """ estimate alpha and sigma """
 
-        pass
+        sol = self.sol
+        par = self.par
+
+        def target(x):
+            par.alpha = x[0]
+            par.sigma = x[1]
+
+            self.solve_wF_vec()
+            self.run_regression()
+            sol.residual = (sol.beta0-par.beta0_target)**2 + (sol.beta1-par.beta1_target)**2
+
+            return sol.residual
+            
+        x0 = [0.8, 0.3]
+
+        # Bounds
+        bounds = ((0.1, 0.99), (0.05,0.99))
+
+        solution = optimize.minimize(target, x0, method="Nelder-Mead", bounds=bounds, tol=1e-9)
+
+        par.alpha = solution.x[0]
+        par.sigma = solution.x[1]
+
+        if do_print:
+                print(f"\u03B1_opt = {par.alpha:6.10f}")
+                print(f"\u03C3_opt = {par.sigma:6.10f}")
+                print(f"Residual_opt = {sol.residual:6.6f}")
+
+        
+    pass
